0e0f1b445805dc822ebf8d8e1b7e122b364036fb
Нозиков Григорий Евгеньевич <NozikovGE@ares.ru>
2025-02-18 16:39:50 +0300

    updated axios interceptorc

--- accounts-frontend/src/main.js
+++ accounts-frontend/src/main.js
@@ -1,25 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from '../router'
+import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ToastPlugin from 'vue-toast-notification'
 import 'vue-toast-notification/dist/theme-bootstrap.css'
-// import axios from 'axios'
-
-// axios.interceptors.response.use(
-//     responce => responce,
-//     error => {
-//         console.log(error)
-//         console.log(error.responce)
-
-//         if(error.responce?.status === 401) {
-            
-//         } else {
-            
-//         }
-//         return Promise.reject(error)
-//     }
-// )
+import './api/interceptors'
 
 const app = createApp(App)
 
